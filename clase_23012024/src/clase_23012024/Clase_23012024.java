6d811b7a921add9da3df93175f66d55a52f35cd5
Claudia Cortes <clau_cortes@unitec.edu>
2024-01-23 18:32:56 -0600

    Uso del scanner en java

--- clase_23012024/src/clase_23012024/Clase_23012024.java
+++ clase_23012024/src/clase_23012024/Clase_23012024.java
@@ -1,21 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package clase_23012024;
 
+import java.util.Scanner;
+
 /**
- *
  * @author Clau
  */
 public class Clase_23012024 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
-        
+        //Scanner 
+        Scanner entrada  = new Scanner(System.in);
+        int nota1 = 0;
+        int nota2=0;
+        int nota3=0;
+        System.out.println("Ingrese la primera nota: ");
+        nota1 = entrada.nextInt();
+         System.out.println("Ingrese la segunda nota: ");
+        nota2 = entrada.nextInt();
+         System.out.println("Ingrese la tercera nota: ");
+        nota3 = entrada.nextInt();
+        double sumaNotas = nota1+nota2+nota3;
+        double promedio = sumaNotas/3;
+        System.out.println("La suma de las notas es "+sumaNotas );
+        System.out.println("El promedio de las notas es "+promedio);
+   
     }
-    
 }
