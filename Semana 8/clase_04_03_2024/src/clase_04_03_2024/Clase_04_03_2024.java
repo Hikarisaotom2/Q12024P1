096991359ccf804d59a23d202341381cf1fe53f0
Claudia Cortes <clau_cortes@unitec.edu>
2024-03-04 18:44:26 -0600

    Clase 04 de Marzo: Objetos y clases

---
+++ Semana 8/clase_04_03_2024/src/clase_04_03_2024/Clase_04_03_2024.java
@@ -0,0 +1,17 @@
+
+package clase_04_03_2024;
+
+/**
+ *
+ * @author Clau
+ */
+public class Clase_04_03_2024 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Semana 8/clase_04_03_2024/src/clase_04_03_2024/Persona.java
@@ -0,0 +1,21 @@
+package clase_04_03_2024;
+
+/**
+ *
+ * @author Clau
+ */
+public class Persona {
+    //Atributos
+    /*Son Variables globales*/
+    String nombre;
+    String Apellido;
+    int edad;
+    String SexoBio;
+    
+    //Métodos 
+    /*e crean igual que como lo haciamos con el main*/
+    public static void inscribirse(){
+        System.out.println("Incribiendo persona......");
+    }
+
+}
